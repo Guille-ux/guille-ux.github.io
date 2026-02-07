66d8cf4dbf0ea02bfbabb76f93563c91ff42df8f
Guillermo leira temes <104579306+Guille-ux@users.noreply.github.com>
2026-02-07 17:02:25 +0000

    Fix variable name for new key in buildKeyBoard

--- kb_layout/main.js
+++ kb_layout/main.js
@@ -5,7 +5,7 @@ function buildKeyBoard() {
   for (let i=1;i<n_keys;i++) {
     const newKey = document.createElement("div");
     newKey.className = "key";
-    key.innerText = "0x" + scancode.toString(16).toUpperCase().padStart(2, '0');
+    newKey.innerText = "0x" + scancode.toString(16).toUpperCase().padStart(2, '0');
     document.appendChild(key);
   }
 }
