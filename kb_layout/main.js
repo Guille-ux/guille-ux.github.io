9161cea7477e9152feacca3cecc3af19806581dd
Guillermo leira temes <104579306+Guille-ux@users.noreply.github.com>
2026-02-07 16:59:55 +0000

    Add keyboard layout generation in main.js

---
+++ kb_layout/main.js
@@ -0,0 +1,13 @@
+const n_keys = 18;
+const keyboard_container = document.getElementById("keyboard");
+
+function buildKeyBoard() {
+  for (let i=1;i<n_keys;i++) {
+    const newKey = document.createElement("div");
+    newKey.className = "key";
+    key.innerText = "0x" + scancode.toString(16).toUpperCase().padStart(2, '0');
+    document.appendChild(key);
+  }
+}
+
+buildKeyBoard();
