3fceddb2f4a352423df03dd243b62798b354e60e
Guillermo leira temes <104579306+Guille-ux@users.noreply.github.com>
2026-02-07 17:25:36 +0000

    Fix key display to use index instead of scancode

--- kb_layout/main.js
+++ kb_layout/main.js
@@ -6,7 +6,7 @@ function buildKeyBoard() {
   for (let i=1;i<n_keys;i++) {
     const newKey = document.createElement("div");
     newKey.className = "key";
-    newKey.innerText = "0x" + scancode.toString(16).toUpperCase().padStart(2, '0');
+    newKey.innerText = "0x" + i.toString(16).toUpperCase().padStart(2, '0');
     const character = document.createElement("input");
     character.className = "character";
     newKey.appendChild(character);
