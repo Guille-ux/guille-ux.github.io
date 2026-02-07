528e09e9fb7d12cc68bde2ca002f351d0d3cb768
Guillermo leira temes <104579306+Guille-ux@users.noreply.github.com>
2026-02-07 17:24:07 +0000

    Update main.js

--- kb_layout/main.js
+++ kb_layout/main.js
@@ -1,13 +1,27 @@
-const n_keys = 18;
+const n_keys = 105;
 const keyboard_container = document.getElementById("keyboard");
+const generate_button = document.getElementById("generate_button");
 
 function buildKeyBoard() {
   for (let i=1;i<n_keys;i++) {
     const newKey = document.createElement("div");
     newKey.className = "key";
     newKey.innerText = "0x" + scancode.toString(16).toUpperCase().padStart(2, '0');
-    document.appendChild(newKey);
+    const character = document.createElement("input");
+    character.className = "character";
+    newKey.appendChild(character);
+    keyboard_container.appendChild(newKey);
   }
 }
 
 buildKeyBoard();
+
+function returnCodes() {
+  resultado.value = "char keyboard_layout[] = [ 0";
+  for (let i=0;i<n_keys;i++) {
+    resultado.value += ", '" + keyboard_container.children[i].children[0].value + "'";
+  }
+  resultado.value += " ];";
+}
+
+generate_button.addEventListener("click", returnCodes, false)
